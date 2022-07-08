75c5c331a8c2d6efc675fc49597a7ad7196a8721
Syamirul Rossman <syamirulrossman@gmail.com>
2022-07-09 01:04:26 +0800

    Login and Log out function

--- dashboard_app/urls.py
+++ dashboard_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('log_in', views.log_in, name='log_in'),
+    path('log_out', views.log_out, name='log_out'),
 ]
